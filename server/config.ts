ac6a6612df9f2bdfdaf8ee0bd87f297902e5c9a8
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2021-12-09 19:06:14 +0530

    Updates

--- server/config.ts
+++
@@ -1,5 +0,0 @@
-import env from 'dotenv'
-import path from 'path'
-export const ENV_PATH = path.join(__dirname, '.env');
-export const DB_USERS_COLL_NAME = 'Users';
-
--- server/index.ts
+++
@@ -1,6 +0,0 @@
-import env from 'dotenv'
-import path from 'path';
-env.config({ path: path.join(__dirname, '.env') });
-import('./app/server').then(server_module => {
-    server_module.default()
-})
