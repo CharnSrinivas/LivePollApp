01857fd3981cc6cb41ae748f515a50774143d4d7
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2021-12-18 21:36:40 +0530

    🌐🌐 Social media 🔗🔗 link preview and Performance improvement

--- view/dist/server/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("ignore-styles");
-require('@babel/register')({
-    ignore: [/(node_module)/],
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript']
-});
-require("./server");
